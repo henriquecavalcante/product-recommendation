43fed334d16ca2936cecfb10e9da748922b48ace
Henrique Cavalcante <henrique.pcavalcante@gmail.com>
2018-06-11 21:29:32 -0400

    Set up initial structure
    Signed-off-by: Henrique Cavalcante <henrique.pcavalcante@gmail.com>

---
+++ backend/db.js
@@ -0,0 +1,17 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';
+
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
